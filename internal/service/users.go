74171d85de02f67363bcbbc212712f2eed1bbd68
kidx45 <kidustessema91@gmail.com>
2026-08-16 17:17:36 +0300

    add and fix

--- internal/service/users.go
+++ internal/service/users.go
@@ -18,9 +18,69 @@ func NewUserService(DB outbound.UserRepository) *UserService {
 	}
 }
 
-func (u *UserService) CreateUser(ctx context.Context, req db.CreateUserParams) (db.User, error) {
-	res, err := u.DB.CreateUser(ctx,req)
+func (u *UserService) CreateUser(ctx context.Context, req db.User) (db.User, error) {
+	// TODO: Fetch users username quickly for the frontend
+	arg := db.CreateUserParams{
+		Username: req.Username,
+		HashedPassword: req.HashedPassword,
+		FullName: req.FullName,
+		Email: req.Email,
+	}
+	res, err := u.DB.CreateUser(ctx, arg)
 	if err != nil {
 		return db.User{}, err
 	}
+	return res,err
+}
+
+func (u *UserService) GetUserByUsername(ctx context.Context, Username string) (db.User, error) {
+	res, err := u.DB.GetUserByUsername(ctx, Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return db.User{}, fmt.Errorf("user not found")
+		}
+		return db.User{}, err
+	}
+	return res, nil
+}
+
+func (u *UserService) UpdateFullNameByUsername(ctx context.Context, Username, FullName string) (db.User, error) {
+	arg := db.UpdateFullNameByUsernameParams{
+		Username: Username,
+		FullName: FullName,
+	}
+	res, err := u.DB.UpdateFullNameByUsername(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return db.User{}, fmt.Errorf("user not found")
+		}
+		return db.User{}, err
+	}
+	return res, nil
+}
+
+func (u *UserService) UpdateUserNameByUsername(ctx context.Context, Username, NewUsername string) (db.User, error) {
+	arg := db.UpdateUserNameByUsernameParams{
+		Username: Username,
+		NewUsername: NewUsername,
+	}
+	res, err := u.DB.UpdateUserNameByUsername(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return db.User{}, fmt.Errorf("user not found")
+		}
+		return db.User{}, err
+	}
+	return res, nil
+}
+
+func (u *UserService) DeleteUserByUsername(ctx context.Context, Username string) error {
+	err := u.DB.DeleteUserByUsername(ctx, Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return fmt.Errorf("user not found")
+		}
+		return err
+	}
+	return nil
 }
--- internal/util/random.go
+++ internal/util/random.go
@@ -6,13 +6,34 @@ func RandomNumber (min,max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
-func RandomString (size int) string {
+func RandomString (size int64) string {
 	s := ""
 	ranges := []int{17,49}
-	for i := 0; i < size; i++ {
+	for i := 0; i < int(size); i++ {
 		num := ranges[rand.Int63n(2)]
 		randomNum := RandomNumber(int64(num),int64(num+25))
 		s += string('0'+randomNum)
 	}
 	return s
+}
+
+func RandomUserName (min, max int) string {
+	size := RandomNumber(int64(min),int64(max))
+	return RandomString(size)
+}
+
+func RandomFullName (min, max int) string {
+	size := RandomNumber(int64(min),int64(max))
+	return RandomString(size)
+}
+
+func RandomEmail (min, max int) string {
+	domain := RandomNumber(int64(min),int64(max))
+	size := RandomNumber(int64(min),int64(max))
+	return RandomString(size) + "@" + RandomString(domain) + ".com"
+}
+
+func RandomPassword (min, max int) string {
+	size := RandomNumber(int64(min),int64(max))
+	return RandomString(size)
 }
